4c67741928750baad9358ea6de2583c8893f1ac4
Omer Erez <omer.e@fellowship.masterschool.com>
2026-06-18 11:31:42 +0300

    database: add module and demo-data seeder.

---
+++ BE/src/database/database.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { ConversationsModule } from '../conversations/conversations.module.js'
+import { MessagesModule } from '../messages/messages.module.js'
+import { UsersModule } from '../users/users.module.js'
+import { DemoDataSeeder } from './demo-data.seeder.js'
+
+@Module({
+  imports: [UsersModule, ConversationsModule, MessagesModule],
+  providers: [DemoDataSeeder],
+})
+export class DatabaseModule {}
